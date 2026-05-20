0b1180de2f52be26e4c78b95e96e9fc91bd33a68
subekshya-s <subekshyasubedi26@gmail.com>
2026-05-20 11:04:20 +0000

    fixed build permissions

--- climaticpeace/climaticpeace/settings.py
+++ climaticpeace/climaticpeace/settings.py
@@ -23,16 +23,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1e640_o!%0cil0p%_mjum96^622wdft2)9g+py#hi1saeyo+p='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
-STATIC_URL = 'static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 
 
 # Application definition
@@ -125,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Media files (user uploads like CVs)
 MEDIA_URL = '/media/'
